94cf4ce1da1d3ca468c338cb2b8dad0b2354aaca
01218279393 <478lemontree@gmail.com>
2026-05-13 17:42:50 +0900

    merge를 위한 push

--- src/components/layout/CategoryNav.jsx
+++ src/components/layout/CategoryNav.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./CategoryNav.css";
 
 function CategoryNav() {
-  return <div>CategoryNav</div>;
+  return (
+    <nav className="nav">
+      <div className="nav-content"></div>
+    </nav>
+  );
 }
 
 export default CategoryNav;
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import SearchBar from "../common/Searchbar";
+import { useNavigate } from "react-router-dom";
+
+import "./Header.css";
+import MainLogo from "../../assets/img/codebomba-logo-Icon.svg";
+import BombLogo from "../../assets/img/bomb-Icon.svg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="header-content">
+        {/* 좌측: 메인 로고 */}
+        <div className="header-left">
+          <img src={MainLogo} className="header-logo-img" />
+        </div>
+
+        {/* 중앙: 검색바 */}
+        <SearchBar />
+
+        {/* 우측: 내강의실, 문제풀이, 로그인(프로필) */}
+        <div className="header-right">
+          <div className="header-text-btn">
+            <p className="header-text">내 강의실</p>
+          </div>
+          <div className="header-text-btn">
+            <p className="header-text">문제풀이</p>
+          </div>
+          <div className="header-btn">
+            <img src={BombLogo} alt="로그인" className="header-bomb-img" />
+            <p className="header-text">로그인</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
